24a9ddf27a21d63326d48ce40a8d89472d0cc80c
tarikh-cell <tarikhmukhtar@gmail.com>
2021-11-10 21:54:43 +0000

    jump

--- MainGameProject/Assets/Scripts/camera.cs
+++ MainGameProject/Assets/Scripts/camera.cs
@@ -8,6 +8,8 @@ public class camera : MonoBehaviour
     private Vector3 offset;
     public float horizontalSpeed = 1.0F;
     public float verticalSpeed = 1.0F;
+    public float rotation;
+
     // Start is called before the first frame update
     void Start(){
         //offset = transform.position;
@@ -15,15 +17,16 @@ public class camera : MonoBehaviour
     }
 
     void Update() {
-        horizontalSpeed += Input.GetAxis("Mouse X");
-        verticalSpeed -= Input.GetAxis("Mouse Y");
+        horizontalSpeed += Input.GetAxis("Mouse X") * 2;
+        verticalSpeed -= Input.GetAxis("Mouse Y") * 2;
         transform.eulerAngles = new Vector3(verticalSpeed, horizontalSpeed, 0.0f);
     }
 
     // Update is called once per frame
     void LateUpdate(){
         transform.position = player.transform.position + offset;
-        //transform.LookAt(player.transform);
+        rotation += Input.GetAxis("Horizontal");
+        //transform.Rotate(0,rotation,0);
     }
 
 }
--- MainGameProject/Assets/Scripts/ship.cs
+++ MainGameProject/Assets/Scripts/ship.cs
@@ -7,17 +7,29 @@ public class ship : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed = 500;
+    public float jumphight = 2.0f;
+    public float gravity = 20.0f;
+    public bool jumping = false;
+    public float rotation;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
+    void OnCollisionStay(){
+	    jumping = false;
+    }
+
     // Update is called once per frame
-    void Update()
-    {
-        Vector3 movement = new Vector3 (moveValue.x , 0.0f , moveValue.y) ;
-        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime );
+    void Update(){
+        Vector3 movement = new Vector3 (moveValue.x , 0.0f , moveValue.y);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+        rotation += Input.GetAxis("Horizontal");
+        transform.Rotate(0,rotation,0);
+        if(Input.GetKey("space") && jumping == false){
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumphight);
+            jumping = true;
+        }
     }
 
     void OnMove ( InputValue value ) {
