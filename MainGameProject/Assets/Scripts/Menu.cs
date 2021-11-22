6a0b8270a6ab22c8c8cbc071e13b1649e3fca546
tarikh-cell <tarikhmukhtar@gmail.com>
2021-11-22 21:24:49 +0000

    Options

--- MainGameProject/Assets/Scripts/Menu.cs
+++ MainGameProject/Assets/Scripts/Menu.cs
@@ -9,4 +9,8 @@ public class Menu : MonoBehaviour
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
 }
