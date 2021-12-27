3c9fa5983d63e787ad82ed4a022bc9a771dfd5d1
tarikh-cell <tarikhmukhtar@gmail.com>
2021-12-27 15:25:40 +0000

    knockback

--- MainGameProject/Assets/Scripts/AI.cs
+++ MainGameProject/Assets/Scripts/AI.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
-public class AI : MonoBehaviour
-{
+// Applied to: Enemies
+// Purpose: Generate AI movement
+public class AI : MonoBehaviour{
+
     [SerializeField]
     private NavMeshAgent agent; // Used to model AI movement
     [SerializeField]
-    private GameObject player; // ges refernece of player to claculate distance from AI
-    private float health;
-    [SerializeField]
-    private Slider displayHealth;
+    private GameObject player; // gets refernece of player to calculate distance from AI
+    //private bool inCombat;
     [SerializeField]
     private Transform attackPoint;
-    [SerializeField]
-    private Animator animation;
     private float shootForce = 100.0f;
     private float wait;
     public GameObject bullet;
@@ -24,59 +21,48 @@ public class AI : MonoBehaviour
     // Start is called before the first frame update
     void Start(){
         agent = GetComponent<NavMeshAgent>(); // gets nav mesh agent
-        animation = GetComponent<Animator>();
-        agent.autoBraking = true; // slows down near nodes   
-        health = 100.0f;
-        displayHealth.value = 1.0f;
-    }
-
-    void OnTriggerEnter(Collider other){
-        if (other.tag == "Melee"){
-            health = health - 5.0f;
-            if(health <= 0.0f){
-                Destroy(this.gameObject);
-            }
-        }
     }
 
     // Update is called once per frame
     void Update(){
-        displayHealth.value = health/100.0f;
-        transform.LookAt(player.transform.position);
-        if(Vector3.Distance(this.transform.position, player.transform.position) <= 1f){
-            Retreat();
-        } else if (Vector3.Distance(this.transform.position, player.transform.position) <= 10f){
-            agent.destination = player.transform.position + new Vector3(2,2,2);
-            Melee();
-        } else if (Vector3.Distance(this.transform.position, player.transform.position) <= 20f){
-            agent.destination = player.transform.position + new Vector3(2,2,2);
-        } else if (Vector3.Distance(this.transform.position, player.transform.position) > 20f){
-            Shoot();
-        } 
-        
+        float distance = Vector3.Distance(this.transform.position, player.transform.position);
+        Move();
+        if (distance > 20){
+            //Shoot();
+        }
     }
 
-    void Retreat(){
-        Debug.Log("Retreat");
+    void OnTriggerEnter(Collider other){
         
+       // force is how forcefully we will push the player away from the enemy.
+        float force = 20;
+    
+        // If the object we hit is the enemy
+        if (other.tag == "Melee"){
+            // Calculate Angle Between the collision point and the player
+            Vector3 dir = other.gameObject.transform.position - this.transform.position;
+            // We then get the opposite (-Vector3) and normalize it
+            dir = -dir.normalized;
+            // And finally we add force in the direction of dir and multiply it by force. 
+            // This will push back the player
+            GetComponent<Rigidbody>().AddForce(dir*force, ForceMode.Impulse);
+        }
     }
 
-    void Melee(){
-        Debug.Log("Melee");
-        animation.SetBool("attacking", true);
+    void Move(){
+        //Positions AI in front of player
+        //With unity world axis, AI will go to different sides of the player - creating unpredictable movement
+        agent.destination = player.transform.position + new Vector3(0,2,2); 
     }
 
     void Shoot(){
         wait -= Time.deltaTime;
-        animation.SetBool("attacking", false);
         if (wait < 0){
             wait = 2.0f;
-            Debug.Log("Shoot");
             GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); // creates new bullets
             Vector3 direction = player.transform.position - attackPoint.position;
             currentBullet.transform.forward = direction.normalized; 
             currentBullet.GetComponent<Rigidbody>().AddForce(direction.normalized * shootForce, ForceMode.Impulse); // FIRE!
-
             Destroy(currentBullet, 1f); // removed from scene after !f
         }
     }
