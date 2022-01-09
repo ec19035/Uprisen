11b668c1cb19cf5bb8c6152121c43013bfa61534
tarikh-cell <tarikhmukhtar@gmail.com>
2022-01-09 22:31:30 +0000

    Tutorial Mode

--- MainGameProject/Assets/Scripts/Tutorial.cs
+++ MainGameProject/Assets/Scripts/Tutorial.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Tutorial : MonoBehaviour
 {
+    public TextMeshProUGUI instruction;
+    private bool start = true;
+    private bool firstCorridor = false;
+    private bool itemCollect = false;
+    private bool secondCorridor = false;
+    private bool jumpInstruction = false;
+    private bool fightInstruction = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        instruction.text = "Welcome to this tutorial. Press WASD or the arrow keys to move around.";
+    }
+
+    void OnTriggerEnter(Collider other){
+        Debug.Log(other.name);
+        if (other.name == "FirstCheckPoint"){
+            firstCorridor = true; 
+        }
+        if (other.name == "SecondCheckPoint"){
+            jumpInstruction = true;
+        }
+        if (other.name == "ThirdCheckPoint"){
+            fightInstruction = true;
+        }
+        if (other.tag == "item"){
+            itemCollect = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) && start){
+            instruction.text = "Proceed outside the corridor to begin.";
+            start = false;
+        }
+        if (firstCorridor){
+            instruction.text = "Pass over that apple there to collect.";
+            firstCorridor = false;
+        }
+        if (itemCollect){
+            instruction.text = "You have just picked up a potion that increases health. To use it press 1";
+            itemCollect = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1) && secondCorridor == false){
+            instruction.text = "Great now head to the next stage.";
+            secondCorridor = true;
+        }
+        if (jumpInstruction){
+            instruction.text = "Press the Space Bar to jump over obstacles and holes. \n Don't worry if you fall, you will automatically respawn at your last checkpoint.";
+            jumpInstruction = false;
+        } 
+        if (fightInstruction){
+            instruction.text = "Click to perform a melee attack and press J to shoot.";
+            fightInstruction = false;
+        }
     }
 }
